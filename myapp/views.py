af2f1eb8c85d28431eb1e2c5decd4a6387dfa0b1
lakshyaakar <lakshyaakar007@gmail.com>
2022-04-29 14:22:04 +0530

    Edit User page added

--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from sqlalchemy import true
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 from django.db.models import Q
 # Create your views here.
 
@@ -163,3 +163,15 @@ def deleteMessage(request,pk):
         return redirect('home')
 
     return render(request, 'delete.html', {'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+    return render(request, 'update-user.html', {'form':form})
